cfa52ca62e19311582794096e6c45b131ab0aa0b
ChristoperBA <bricenoc506@gmail.com>
2023-02-11 19:16:13 -0600

    Home and Login

---
+++ Shop/src/main/java/com/tienda/entity/Pais.java
@@ -0,0 +1,40 @@
+
+package com.tienda.entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+
+
+@Entity
+@Table(name="paises")
+public class Pais implements Serializable {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private long id;
+    private String pais;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+       
+ 
+}
+
